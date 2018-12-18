2fe5d3777b9be57155f0859cda622dde89e65fcc
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-12-18 20:21:55 +0100

    more aditions testing etc

--- src/MisplacedItemDetection.cpp
+++ src/MisplacedItemDetection.cpp
@@ -38,12 +38,50 @@ private:
   int imgIdx_, modelIdx_;
   bool testMode_;
 
+
+  enum {
+    CANNY,
+    MASK,
+    BIN,
+    MATCHES,
+    HIST
+  } displayMode;
+
+  cv::Mat canny_, bin_, mask_, hist_;
+
   std::stringstream positiveMatch, negativeMatch;
 public:
 
-  MisplacedItemDetection(): DrawingAnnotator(__func__), imgIdx_(0), modelIdx_(0), testMode_(false)
+  MisplacedItemDetection(): DrawingAnnotator(__func__), imgIdx_(0), modelIdx_(0), testMode_(false), displayMode(BIN)
   {
     detector = cv::ORB::create();
+  }
+
+  bool callbackKey(const int key, const Source source)
+  {
+    switch(key) {
+    case 'b':
+    case 'B':
+      displayMode = BIN;
+      return true;
+    case 'c':
+    case 'C':
+      displayMode = CANNY;
+      return true;
+    case 'm':
+    case 'M':
+      displayMode = MASK;
+      return true;
+    case 'O':
+    case 'o':
+      displayMode = MATCHES;
+      return true;
+    case 'H':
+    case 'h':
+      displayMode = HIST;
+      return true;
+    }
+    return false;
   }
 
   void readInfoFromJson(const std::string &pathToJson, cv::Rect &rect, std::string &gtin)
@@ -64,12 +102,37 @@ public:
 
   cv::Mat getMaskForModelImage(const cv::Mat &imgGrey)
   {
-    cv::Mat bin;
+    cv::Mat bin, canny;
     cv::threshold(imgGrey, bin, 10, 255, cv::THRESH_BINARY);
     cv::Mat element = cv::getStructuringElement(0, cv::Size(11, 11), cv::Point(5, 5));
     cv::morphologyEx(bin, bin, cv::MORPH_ERODE, element);
-    //      cv::Ptr<cv::SimpleBlobDetector> = cv::SimpleBlobDetector::create();
-    return bin;
+    bin_ = bin.clone();
+    int cannyThreshold = 10;
+    cv::Canny(bin, canny, cannyThreshold, cannyThreshold * 2);
+    cv::morphologyEx(canny, canny, cv::MORPH_DILATE, element);
+    canny_ = canny.clone();
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<std::vector<cv::Point> > contoursToKeep;
+    std::vector<cv::Vec4i> hierarchy;
+
+    cv::findContours(canny, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+    outInfo("Found " << contours.size() << " contours");
+    cv::Mat newImg = cv::Mat::zeros(imgGrey.rows, imgGrey.cols, CV_8U);
+
+
+    for(int idx = 0; idx < contours.size(); idx ++) {
+      double area = cv::contourArea(contours[idx]);
+      if(area > 0.1 * imgGrey.cols * imgGrey.rows && hierarchy[idx][3] == -1) {
+        contoursToKeep.push_back(contours[idx]);
+      }
+    }
+
+    cv::drawContours(newImg, contoursToKeep, -1, cv::Scalar(255));
+    cv::fillPoly(newImg, contoursToKeep, cv::Scalar(255));
+
+    cv::morphologyEx(newImg, newImg, cv::MORPH_ERODE, element);
+    mask_ = newImg.clone();
+    return mask_;
   }
 
   TyErrorId initialize(AnnotatorContext &ctx)
@@ -131,10 +194,50 @@ public:
     return UIMA_ERR_NONE;
   }
 
+
+  /**
+   * @brief matchModelToFacing perform matching and getSimilarity score
+   * @return
+   */
+  double matchModelToFacing()
+  {
+
+  }
+
   TyErrorId destroy()
   {
     outInfo("destroy");
     return UIMA_ERR_NONE;
+  }
+
+  cv::Mat calcHistogram(cv::Mat &imgGrey, cv::Mat mask = cv::Mat())
+  {
+    /// Establish the number of bins
+    int histSize = 64;
+    /// Set the ranges ( for B,G,R) )
+    float range[] = { 0, 256 } ;
+    const float *histRange = { range };
+
+    cv::Mat histModel, histModelNormed;
+    cv::calcHist(&imgGrey, 1, 0, mask, histModel, 1, &histSize, &histRange, true, false);
+    cv::normalize(histModel, histModelNormed, 0, 1.0, cv::NORM_MINMAX, -1, cv::Mat());
+
+
+    int hist_w = 512;
+    int hist_h = 100;
+    int bin_w = cvRound((double) hist_w / histSize);
+    cv::normalize(histModel, histModel, 0, hist_h, cv::NORM_MINMAX, -1, cv::Mat());
+
+    cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    for(int i = 1; i < histSize; i++) {
+      cv::line(histImage, cv::Point(bin_w * (i - 1), hist_h - cvRound(histModel.at<float>(i - 1))) ,
+               cv::Point(bin_w * (i), hist_h - cvRound(histModel.at<float>(i))),
+               cv::Scalar(255, 0, 0), 2, 8, 0);
+
+    }
+    hist_.push_back(histImage);
+    return histModelNormed;
   }
 
   TyErrorId processWithLock(CAS &tcas, ResultSpecification const &res_spec)
@@ -142,7 +245,7 @@ public:
     outInfo("process start");
     rs::StopWatch clock;
     cv::Mat rgb;
-
+    hist_ = cv::Mat();
     cv::Mat facingImg;
     cv::Rect facingRect;
     std::string gTinOfFacing;
@@ -160,6 +263,7 @@ public:
         if(detections.empty()) continue;
 
         rs::Detection &det = detections[0];
+        det.confidence.set(0.77);
 
         if(det.source()  == "FacingDetection") {
           rs::ImageROI roi = h.rois();
@@ -180,15 +284,17 @@ public:
     bfs::path savefolderPath(ros::package::getPath("rs_refills") + "/test_results/" + gTinOfFacing + "/");
     if(!bfs::exists(savefolderPath)) boost::filesystem::create_directory(savefolderPath);
 
-    cv::GaussianBlur(facingImg, facingImg, cv::Size(5, 5), 0);
+    cv::Mat facingHist = calcHistogram(facingImg);
+    cv::GaussianBlur(facingImg, facingImg, cv::Size(9, 9), 0);
 
     std::vector<cv::KeyPoint> keypoints1, keypoints2;
+    std::vector<cv::Point> goodKp1, goodKp2;
     cv::Mat descriptors1, descriptors2;
 
     //keypoints for the facing
     detector->detectAndCompute(facingImg, cv::noArray(), keypoints1, descriptors1);
 
-    outInfo("Detected " << keypoints1.size() << " keypoints in facjng image");
+    outInfo("Detected " << keypoints1.size() << " keypoints in facing image");
 
     std::map<std::string, std::vector<std::string> >::iterator it = gtinToImages_.begin();
     it = std::next(it, modelIdx_++);
@@ -200,16 +306,19 @@ public:
     std::vector<std::string> &modelImages = it->second;
 
     cv::Mat modelImage;
+    cv::Mat modelHist;
     for(auto mi : modelImages) {
       outInfo("Gtin: " << gTinOfFacing << " model image: " << mi);
       modelImage = cv::imread(mi, cv::IMREAD_GRAYSCALE);
       cv::Mat modelMask = getMaskForModelImage(modelImage);
-      cv::GaussianBlur(modelImage, modelImage, cv::Size(5, 5), 0);
+      modelHist = calcHistogram(modelImage, modelMask);
+      cv::GaussianBlur(modelImage, modelImage, cv::Size(9, 9), 0);
       detector->detectAndCompute(modelImage, modelMask, keypoints2, descriptors2);
       break;
     }
     outInfo("Detected " << keypoints2.size() << " keypoints in model image");
 
+    double histDist = cv::compareHist(facingHist, modelHist, CV_COMP_CORREL);
 
     cv::Ptr<cv::BFMatcher> matcher = cv::BFMatcher::create(cv::NORM_HAMMING, true);
     std::vector<cv::DMatch> matches;
@@ -222,23 +331,29 @@ public:
       return  a.distance < b.distance;
     });
 
+
+
     std::vector<char> mask(matches.size(), 0);
-    for(int i = 0; i < 50 && i < mask.size(); ++i) {
+    for(int i = 0; i < 20 && i < mask.size(); ++i) {
       mask[i] = 1;
     }
 
 
     float avgDist = 0.0;
     uint8_t mi = 1;
-    for(; mi < matches.size() && mi <= 50; ++mi) {
+    for(; mi < matches.size() && mi <= 20; ++mi) {
       outInfo(1 - (matches[mi - 1].distance / 256));
       avgDist += 1 - (matches[mi - 1].distance / 256);
+      goodKp1.push_back(keypoints1[matches[mi].queryIdx].pt);
+      goodKp2.push_back(keypoints2[matches[mi].queryIdx].pt);
     }
 
+    cv::Mat homograpy = cv::findHomography(goodKp1,goodKp2,CV_RANSAC);
+    outInfo(homograpy);
 
     std::ofstream histFile;
     if(modelGtin == gTinOfFacing) {
-      histFile.open("01match.csv", std::ostream::out);
+      histFile.open(savefolderPath.string() + "01match.csv", std::ostream::out);
     }
     else {
       histFile.open(savefolderPath.string() + modelGtin + ".csv", std::ostream::out);
@@ -250,7 +365,8 @@ public:
     histFile.close();
 
     avgDist /= mi;
-    outInfo("Average dist of best 100 matches: " << avgDist);
+    outInfo("Average dist of best 20 matches: " << avgDist);
+    outInfo("Histogram correlation: " << histDist);
     if(modelGtin == gTinOfFacing) {
       positiveMatch << avgDist << ",";
       outError("THIS IS THE REAL MATCH");
@@ -280,9 +396,27 @@ public:
 
   void drawImageWithLock(cv::Mat &disp)
   {
-    disp = disp_.clone();
+    switch(displayMode) {
+    case BIN:
+      disp = bin_.clone();
+      break;
+    case CANNY:
+      disp = canny_.clone();
+      break;
+    case MASK:
+      disp = mask_.clone();
+      break;
+    case MATCHES:
+      disp = disp_.clone();
+      break;
+    case HIST:
+      disp = hist_.clone();
+      break;
+    default:
+      disp = disp_.clone();
+      break;
+    }
   }
-
 };
 
 // This macro exports an entry point that is used to create the annotator.
